14b3851dba9b95bfefd4804c3cc6bc26962a2be3
withewolf-dev <itskashyap26@gmail.com>
2021-08-16 11:06:51 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Footer from "./Footer";
 
 function App() {
+  const [date, setdate] = useState("");
+  const [luckynum, setluckynum] = useState("");
+  const [result, setresult] = useState("");
+
+  const checkNum = () => {
+    const sum = getDateSum();
+
+    if (sum % luckynum === 0) {
+      setresult("lucky number ✨");
+    } else {
+      setresult("not lucky number 🥺");
+    }
+  };
+  const getDateSum = () => {
+    const data = date.split("-");
+
+    const sum = parseInt(data[0]) + parseInt(data[1]) + parseInt(data[2]);
+
+    return sum;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Is your birthday lucky number?</h1>
+      <h3>Date of birth</h3>
+      <input
+        onChange={(e) => {
+          setdate(e.target.value);
+        }}
+        type="date"
+        id="birthday"
+        name="birthday"
+      />
+      <h3>Lucky number</h3>
+
+      <input
+        type="number"
+        onChange={(e) => {
+          setluckynum(e.target.value);
+        }}
+      />
+      <br />
+      <button className="button" onClick={checkNum}>
+        check number
+      </button>
+      {result !== null && <h1>{result}</h1>}
+      <Footer />
     </div>
   );
 }
